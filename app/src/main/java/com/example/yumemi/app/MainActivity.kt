2826f3c87652b19c60d63f625dcfd9e40f25287b
Steve Madsen <steve.madsen@zeppelin.co.jp>
2020-09-09 09:36:58 +0900

    Introduce viewModelFactory

--- app/src/main/java/com/example/yumemi/app/MainActivity.kt
+++ app/src/main/java/com/example/yumemi/app/MainActivity.kt
@@ -13,16 +13,24 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProvider
+import com.example.yumemi.app.ui.home.HomeViewModel
+import com.example.yumemi.app.ui.home.HomeViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        homeViewModel =
+            ViewModelProvider(this, HomeViewModelFactory(this))
+                .get(HomeViewModel::class.java)
 
         val fab: FloatingActionButton = findViewById(R.id.fab)
         fab.setOnClickListener { view ->
--- app/src/main/java/com/example/yumemi/app/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/yumemi/app/ui/home/HomeViewModel.kt
@@ -1,13 +1,27 @@
 package com.example.yumemi.app.ui.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.*
+import com.example.yumemi.data.net.GithubImpl
+import com.example.yumemi.domain.model.Contributor
+import com.example.yumemi.domain.result.GithubRespBase
+import kotlinx.coroutines.launch
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(val activity: FragmentActivity) : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+}
+
+class HomeViewModelFactory(private val activity: FragmentActivity) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HomeViewModel(activity) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
